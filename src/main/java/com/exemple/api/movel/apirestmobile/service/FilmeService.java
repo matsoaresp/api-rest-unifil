3a16a6bc9b42b4c4a1ed1f6f134c22743ad84f1d
Mateus Prado <maateussp@gmail.com>
2025-11-24 09:50:26 -0300

    Implementing code of service package

---
+++ src/main/java/com/exemple/api/movel/apirestmobile/service/FilmeService.java
@@ -0,0 +1,31 @@
+package com.exemple.api.movel.apirestmobile.service;
+
+import com.exemple.api.movel.apirestmobile.entity.Filme;
+import com.exemple.api.movel.apirestmobile.repository.FilmeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FilmeService {
+
+    @Autowired
+    private FilmeRepository filmeRepository;
+
+    public Filme save (Filme filme){
+        return filmeRepository.save(filme);
+    }
+
+    public List<Filme> findAll() {
+        return filmeRepository.findAll();
+    }
+
+    public Filme findById(Long id){
+        return filmeRepository.findById(id).orElse(null);
+    }
+
+    public void delete(Long id) {
+        filmeRepository.deleteById(id);
+    }
+}
