9ad0d55d00efdee26616d8d1dffc83f5a4603ea4
Mateus Prado <maateussp@gmail.com>
2025-11-25 08:32:06 -0300

    Implementing service repository and relations in database

--- src/main/java/com/exemple/api/movel/apirestmobile/repository/DiretorRepository.java
+++ src/main/java/com/exemple/api/movel/apirestmobile/repository/DiretorRepository.java
@@ -1,9 +1,10 @@
 package com.exemple.api.movel.apirestmobile.repository;
 
+import com.exemple.api.movel.apirestmobile.entity.Diretor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DiretorRepository extends JpaRepository {
+public interface DiretorRepository extends JpaRepository<Diretor, Long> {
 
 }
