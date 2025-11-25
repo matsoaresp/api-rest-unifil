141e2160b2af0f21a8946a5f0387b34ee9a1b708
Mateus Prado <maateussp@gmail.com>
2025-11-25 09:09:33 -0300

    Correction of validations of http requisitions

--- src/main/java/com/exemple/api/movel/apirestmobile/repository/FilmeRepository.java
+++ src/main/java/com/exemple/api/movel/apirestmobile/repository/FilmeRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FilmeRepository  extends JpaRepository<Filme, Long> {
-
+    boolean existsByDiretorId(Long id);
 
 
 }
