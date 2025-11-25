cd12309ce4c94983058b3b07963981a9733fada1
Mateus Prado <maateussp@gmail.com>
2025-11-25 08:16:59 -0300

    Additing table diretor

---
+++ src/main/java/com/exemple/api/movel/apirestmobile/repository/DiretorRepository.java
@@ -0,0 +1,9 @@
+package com.exemple.api.movel.apirestmobile.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiretorRepository extends JpaRepository {
+
+}
